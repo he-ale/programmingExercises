56ede7d39bc15ceb8b97a4886616be52efbcd67f
he-ale <he.ale.cladera@gmail.com>
2026-02-04 11:10:19 -0400

    InterleavingStringJava

---
+++ leetcode/javaLanguage/InterleavingString.java
@@ -0,0 +1,40 @@
+public class InterleavingString {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        Boolean dp[][] = new Boolean[s1.length()+1][s2.length()+1];
+        if(s1.length()+s2.length()!=s3.length())return false; 
+        return helper(0,0,s1,s2,s3,dp);
+    }
+
+    public boolean helper(int i, int j, String s1, String s2, String s3, Boolean dp[][]){
+        if(i==s1.length() && j==s2.length()){
+            return true;
+        }
+        int k = i+j;
+
+        if(dp[i][j]!=null){
+            return dp[i][j];
+        }
+
+        boolean ans = false;
+
+        if(i<s1.length() && s1.charAt(i)==s3.charAt(k)){
+            boolean opt = helper(i+1,j,s1,s2,s3,dp);
+            ans = ans || opt;
+        }
+        if(j<s2.length() && s2.charAt(j)==s3.charAt(k)){
+            boolean opt = helper(i,j+1,s1,s2,s3,dp);
+            ans = ans||opt;
+        }
+
+        return dp[i][j]=ans;
+    }
+    
+    public static void main(String[] args) {
+        InterleavingString solution= new InterleavingString();
+        System.out.println(solution.isInterleave("aabcc", "dbbca", "aadbbcbcac"));
+        System.out.println(solution.isInterleave("", "", ""));
+        // System.out.println(solution.isInterleave("aabcc", "dbbca", "aadbbbaccc"));
+        // System.out.println(solution.isInterleave("a", "", "c"));
+        // System.out.println(solution.isInterleave("aaa", "bbb", "bbbbbb"));
+    }
+}
