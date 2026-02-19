0943d464fd57d79450b7233a4672120df098f2d0
he-ale <he.ale.cladera@gmail.com>
2026-02-19 14:58:18 -0400

    ContainerWithMostWaterPython

---
+++ leetcode/pythonLanguage/ContainerWithMostWater.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+
+            h = min(height[left], height[right])
+            w = right - left
+            area = h * w
+
+            max_area = max(max_area, area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+solution= Solution()
+# print(solution.maxArea([1,8,6,2,5,4,8,3,7]))
+# print(solution.maxArea([1,1]))
+print(solution.maxArea([1,2,4,3]))
